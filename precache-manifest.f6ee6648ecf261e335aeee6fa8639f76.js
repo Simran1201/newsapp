73c2ee85619d76a6f40acd8e5958530fb7337b00
Simran Prasad <71308532+Simran1201@users.noreply.github.com>
2022-08-04 11:45:10 +0530

    Updates

---
+++ precache-manifest.f6ee6648ecf261e335aeee6fa8639f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "449e1945d6ae4a6c2417",
+    "url": "/newsapp/static/css/main.f349c538.chunk.css"
+  },
+  {
+    "revision": "449e1945d6ae4a6c2417",
+    "url": "/newsapp/static/js/main.449e1945.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/newsapp/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "37fd148392880842529d",
+    "url": "/newsapp/static/js/2.37fd1483.chunk.js"
+  },
+  {
+    "revision": "f4f56b5cb7fb98ca7718",
+    "url": "/newsapp/static/js/runtime~main.f4f56b5c.js"
+  },
+  {
+    "revision": "5fc6792ba0f092527ad98102efd65e86",
+    "url": "/newsapp/index.html"
+  }
+];
